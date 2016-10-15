637e73322ab304c0727be68043c9c9cc8e851d64
Raphael Fernando Paez <Raphael Fernando Paez>
2016-10-14 23:59:50 -0300

    testando

--- corta_string_php.php
+++ corta_string_php.php
@@ -1,20 +1,34 @@
 <?php
-function cortaString( $string , $limit ) {
+
+class Str{
+    function __construct($str){
+        $this->value = $str;
+        $this->length = strlen($str);
+        ..
+    }
+
+    function __toString(){
+        return $this->value;
+    }
+    function cortaString( $string , $limit ) {
 	
-	if($limit != ""){
-			$fim = $inicio + $limit;
-	}else{
-		return "Determinar um tamanho para corte";
+		if($limit != ""){
+				$fim = $inicio + $limit;
+		}else{
+			return "Determinar um tamanho para corte";
+		}
+		$divide_frase = explode(" ", $string);
+		$length = strlen($string);
+		$stringResult .= $divide_frase[0];
+		for($i=$inicio;$i<=$fim;$i++){
+			$stringResult .= $divide_frase[$i]." ";
+		}
+		if(strlen($stringResult) > 0){
+			$stringResult .= " ...";
+		}
+		return $stringResult;
 	}
-	$divide_frase = explode(" ", $string);
-	$length = strlen($string);
-	$stringResult .= $divide_frase[0];
-	for($i=$inicio;$i<=$fim;$i++){
-		$stringResult .= $divide_frase[$i]." ";
-	}
-	if(strlen($stringResult) > 0){
-		$stringResult .= " ...";
-	}
-	return $stringResult;
+	
 }
+
 ?>
